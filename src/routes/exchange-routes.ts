9905e45cedf287a1c6795e7e242dd12ec00a33d4
punjira <saeedwmail@gmail.com>
2021-10-19 11:14:37 +0330

    add doc markup

--- src/routes/exchange-routes.ts
+++ src/routes/exchange-routes.ts
@@ -2,6 +2,14 @@ import { Router } from 'express';
 const router = Router();
 import { getCoinsInfoRequest } from '../controllers/coin-info-controller';
 
+/**
+ * @api {get} /exchange Get all available exchanges
+ * @apiName GetExchanges
+ * @apiGroup CoinInfo
+ *
+ * @apiSuccess  {Object[]} exchange         List of exchanges
+ * @apiSuccess  {String}   exchange.ticker  ticker ex: btcusdt
+ */
 router.get('/', getCoinsInfoRequest);
 
 export default router;
