2cb104c56b6dc8247dda29a4b26ecaba35ecb917
punjira <saeedwmail@gmail.com>
2021-10-19 11:14:20 +0330

    add nats client

--- src/nats/nats-helper.ts
+++ src/nats/nats-helper.ts
@@ -1,3 +1,40 @@
 /**
  * nats client api
  */
+
+import nats from 'node-nats-streaming';
+import { randomBytes } from 'crypto';
+
+export const EXCHANGE_UPDATE_EVENT = 'EXCHANGE_UPDATE';
+
+export const natsClient = (function () {
+     class NatsClient {
+          stan: nats.Stan;
+          constructor() {
+               this.stan = nats.connect(
+                    'cryptograph',
+                    randomBytes(4).toString('hex'),
+                    {
+                         url: process.env.NATS_URL,
+                    }
+               );
+          }
+          getClient() {
+               return this.stan;
+          }
+          publishMessage(topic, message) {
+               this.stan.publish(topic, JSON.stringify(message), () => {
+                    // do logging
+               });
+          }
+     }
+     let instance: NatsClient;
+     return {
+          getInstance: () => {
+               if (!instance) {
+                    instance = new NatsClient();
+               }
+               return instance;
+          },
+     };
+})();
