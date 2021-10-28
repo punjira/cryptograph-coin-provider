8aec82d55dd33a78b42130e469f96142616b03f3
punjira <saeedwmail@gmail.com>
2021-10-28 15:07:19 +0330

    add top ten coins route

---
+++ src/controllers/featured-coins-controller.ts
@@ -0,0 +1,15 @@
+import { InternalServerError } from '@cryptograph-app/error-handlers';
+import { Coin, CoinModel } from '../models/coin-info';
+
+export function getTopTen(req, res, next) {
+     CoinModel.find({})
+          .limit(10)
+          .exec(function (err, result) {
+               if (err) {
+                    throw new InternalServerError();
+               }
+               return res.status(200).json({
+                    data: result,
+               });
+          });
+}
