ffec4b81f46eefd58839ede85e3720aa88a834f8
punjira <saeedwmail@gmail.com>
2021-11-02 10:03:16 -0400

    add search controllers

---
+++ src/controllers/search-controller.ts
@@ -0,0 +1,17 @@
+import { InternalServerError } from '@cryptograph-app/error-handlers';
+import { Coin, CoinModel } from '../models/coin-info';
+
+export function simpleSearch(req, res, next) {
+     const keyword = req.params.keyword;
+     const reg = new RegExp(keyword);
+     CoinModel.find({ ticker: { $regex: reg, $options: 'i' } })
+          .limit(5)
+          .exec((err, result) => {
+               if (err) {
+                    throw new InternalServerError();
+               }
+               return res.status(200).json({
+                    data: result,
+               });
+          });
+}
