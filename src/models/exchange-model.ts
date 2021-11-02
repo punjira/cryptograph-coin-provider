671d10eb9d215fbb5aaae862204ff228a047956f
punjira <saeedwmail@gmail.com>
2021-11-02 10:01:36 -0400

    use external exchange schema

--- src/models/exchange-model.ts
+++ src/models/exchange-model.ts
@@ -1,21 +1,11 @@
 import mongoose from 'mongoose';
 
-export interface Exchange {
-     ticker: string;
-}
-
-const exchangeSchema = new mongoose.Schema<Exchange>({
-     ticker: {
-          type: String,
-          required: true,
-          unique: true,
-     },
-});
+import { Exchange, ExchangeSchema } from '@cryptograph-app/shared-models';
 
 const createExchangeDocumentFromStringArray = (data: string[]): Exchange[] => {
      return data.map((el) => ({ ticker: el }));
 };
 
-const ExchangeModel = mongoose.model<Exchange>('exchange', exchangeSchema);
+const ExchangeModel = mongoose.model<Exchange>('exchange', ExchangeSchema);
 
 export { ExchangeModel, createExchangeDocumentFromStringArray };
