a1a822924aafd6ca87b102a039eac901a993ee8f
punjira <saeedwmail@gmail.com>
2021-11-07 09:12:37 -0500

    add whitelisted coins

---
+++ src/helpers/whitelist.ts
@@ -0,0 +1,46 @@
+/**
+ *
+ */
+
+export default function getWhiteList(): string[] {
+     return [
+          'btcusdt',
+          'ethusdt',
+          'bnbusdt',
+          'solusdt',
+          'adausdt',
+          'xrpusdt',
+          'dotusdt',
+          'shibusdt',
+          'dogeusdt',
+          'lunausdt',
+          'linkusdt',
+          'ltcusdt',
+          'maticusdt',
+          'uniusdt',
+          'algousdt',
+          'bchusdt',
+          'atomusdt',
+          'xlmusdt',
+          'vetusdt',
+          'axsusdt',
+          'icpusdt',
+          'filusdt',
+          'trxusdt',
+          'fttusdt',
+          'thetausdt',
+          'etcusdt',
+          'ftmusdt',
+          'hbarusdt',
+          'nearusdt',
+          'egldusdt',
+          'xtzusdt',
+          'xmrusdt',
+          'grtusdt',
+          'runeusdt',
+          'eosusdt',
+          'cakeusdt',
+          'arusdt',
+          'klayusdt',
+     ];
+}
