c1feb31452af55f82c593ce0c6c513b6c0f56d66
punjira <saeedwmail@gmail.com>
2021-11-07 09:12:54 -0500

    add ofline exchanges

---
+++ src/lib/offline-exchange.ts
@@ -0,0 +1,36 @@
+import { logger, LOG_LEVELS } from '../../winston';
+import fixedExchanges from '../helpers/whitelist';
+import {
+     createExchangeDocumentFromStringArray,
+     ExchangeModel,
+} from '../models/exchange-model';
+import { natsClient } from '../nats/nats-helper';
+import { EXCHANGE_LIST_REPLY } from '../nats/subscription';
+import getCoinInformation from './coin-info';
+
+export default (async function () {
+     logger(
+          LOG_LEVELS.INFO,
+          'reading exchanges from offline json file and populating database'
+     );
+     const Documents = createExchangeDocumentFromStringArray(fixedExchanges());
+     ExchangeModel.insertMany(Documents, { ordered: false })
+          .then((data) => {
+               /**
+                * notify others
+                */
+               natsClient
+                    .getInstance()
+                    .getClient()
+                    .publish(
+                         EXCHANGE_LIST_REPLY,
+                         JSON.stringify(
+                              fixedExchanges().map((el) => ({
+                                   ticker: el,
+                              }))
+                         )
+                    );
+               return getCoinInformation(data);
+          })
+          .catch((err) => {});
+})();
